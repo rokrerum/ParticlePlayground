d32fec43947e8885b7913efec61a5fad711a37c4
rokrerum <132296840+rokrerum@users.noreply.github.com>
2026-03-02 22:26:53 +0100

    Refactor pygame_gui.py for improved structure

--- pygame_gui.py
+++ pygame_gui.py
@@ -5,17 +5,18 @@ COLOR_INACTIVE = pygame.Color('lightskyblue3')
 COLOR_ACTIVE = pygame.Color('dodgerblue2')
 FONT = pygame.font.Font(None, 32)
 
+
 class Gui:
     def __init__(self):
         pass
-    
+
     def draw_text(x, y, text, font, color, surface):
         textobj = font.render(text, 1, color)
         textrect = textobj.get_rect()
         textrect.topleft = (x, y)
         surface.blit(textobj, textrect)
-       
-       
+
+
 class button:
     def __init__(self, size, text, font, color, display):
         self.rect = pygame.Rect(size[0], size[1], size[2], size[3])
@@ -24,19 +25,17 @@ class button:
         self.color = color
         self.display = display
 
-    
     def add_button(self):
         pygame.draw.rect(self.display, self.color, self.rect)
         Gui.draw_text(self.rect[0], self.rect[1], self.text, self.font, (100, 100, 100), self.display)
-        
-    
+
     def button_press(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             # If the user clicked on the input_box rect.
             if self.rect.collidepoint(event.pos):
                 return self.text
 
-                    
+
 class InputBox:
     def __init__(self, x, y, w, h, var_name, text=''):
         self.rect = pygame.Rect(x, y, w, h)
@@ -46,7 +45,6 @@ class InputBox:
         self.active = False
         self.submitted = None
         self.var_name = var_name
-
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -70,16 +68,14 @@ class InputBox:
                 # Re-render the text.
                 self.txt_surface = FONT.render(self.text, True, self.color)
 
-
     def update(self):
         # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width()+10)
+        width = max(200, self.txt_surface.get_width() + 10)
         self.rect.w = width
-
 
     def draw(self, screen):
         # Blit the text.
-        pygame.draw.rect(screen, (20, 20, 80), self.rect,)
-        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pygame.draw.rect(screen, (20, 20, 80), self.rect, )
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
         # Blit the rect.
         pygame.draw.rect(screen, self.color, self.rect, 2)
